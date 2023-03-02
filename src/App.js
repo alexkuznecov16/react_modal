ce84d6670d41a0f5fa2f05b8dc1a24d057540639
Alexander Kuznecov <alexander.kuznecov16@gmail.com>
2023-03-02 17:21:12 +0200

    dfdsg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './main.scss';
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const onClickClose = () => {
+    setOpen(false);
+  }
+
+  const onClickOpen = () => {
+    setOpen(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="Wrapper">
+        <div className="Wrapper__inner">
+        <h2 className='Wrapper__name'>Modal</h2>
+        <button onClick={onClickOpen} className='Wrapper__button open-modal'>Open modal</button>
+        </div>
+        {
+          open && (
+            <div className='Modal'>
+              <div className="Modal__window">
+                <h3 className='Modal__title'>Modal window</h3>
+                <p className='Modal__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi vel voluptates, ullam rerum expedita aperiam inventore eos non impedit doloremque.</p>
+                <button onClick={onClickClose} className='Modal__close close-modal'>X</button>
+              </div>
+            </div> 
+          )
+        }
+      </div>
+      <footer>
+        &copy; Alexander Kuznecov 2023
+      </footer>
+    </body>    
   );
 }
 
